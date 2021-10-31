ee2242fcc54183d7ef9b195a14c2a6df248cb171
VidhyaDevi G <VidhyaDevi.G@xome.com>
2021-10-31 20:49:33 +0530

    Code for generating unique codes

---
+++ ProblemSolving/Utility.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProblemSolving
+{
+    class Utility
+    {
+        static string Generate6digitUniqueCode()
+        {
+            Random ran = new Random();
+            string characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+            int UniqueCodeLength = 6;
+            StringBuilder result = new StringBuilder();
+
+            for (int i = 0; i < UniqueCodeLength; i++)
+            {
+                int pos = ran.Next(characters.Length);
+                result.Append(characters.ElementAt(pos));
+            }
+            return result.ToString();
+        }
+    }
+}
